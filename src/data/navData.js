7bede088fe572422eb814d4c212f6d7ec0c2978b
LeotheProdu <leovpc@gmail.com>
2023-09-13 05:20:22 -0600

    he creado compoente Link, he avanzado los estilos

---
+++ src/data/navData.js
@@ -0,0 +1,39 @@
+export const navData = [
+    {
+        name: 'About Me',
+        path: '/about/',
+        icon: 'person',
+    },
+    {
+        name: 'Articles',
+        path: '/blog/',
+        icon: 'article',
+    },
+    {
+        name: 'Work',
+        path: '/projects/',
+        icon: 'work',
+    },
+    {
+        name: 'Contact',
+        path: '/contact/',
+        icon: 'contact_page',
+    },
+];
+export const social = [
+    {
+        name: 'GitHub',
+        path: 'https://github.com/Leotheprodu',
+        icon: 'github',
+    },
+    {
+        name: 'LinkedIn',
+        path: 'https://www.linkedin.com/in/leotheprodu/',
+        icon: 'linkedin',
+    },
+    {
+        name: 'Twitter',
+        path: 'https://twitter.com/leonardosays03',
+        icon: 'x',
+    },
+];
