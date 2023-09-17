dae4fc00d61f6ef24cf6ea7a763a5970b9359c76
LeotheProdu <leovpc@gmail.com>
2023-09-17 00:11:18 -0600

    corregido la logica del dark mode, agregada manejo del estado con nanostore y react

--- src/stores/store.js
+++ src/stores/store.js
@@ -2,11 +2,11 @@ import { atom } from 'nanostores';
 
 //configuracion de darkmode
 const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-export const darkModeStore = atom(false);
+export const $darkModeStore = atom(darkModeMediaQuery.matches);
 darkModeMediaQuery.addEventListener('change', (e) => {
     if (e.matches) {
-        darkModeStore.set(true);
+        $darkModeStore.set(true);
     } else {
-        darkModeStore.set(false);
+        $darkModeStore.set(false);
     }
 });
