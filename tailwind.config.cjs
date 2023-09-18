7361ef0ddf4af6d76958b4c091e35cd61d36aa28
LeotheProdu <leovpc@gmail.com>
2023-09-18 01:34:01 -0600

    he corregido el error de sccs en archivo .astro y tailwind postcss.config.js

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    plugins: [require('tailwindcss/nesting')],
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
         extend: {
@@ -9,6 +10,5 @@ module.exports = {
             },
         },
     },
-    plugins: [],
     darkMode: 'class',
 };
