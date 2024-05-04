c2494c5f6b5a0b44a1ab961040ff9ff4a9888fa3
Leonardo Serrano Alfaro <leovpc@gmail.com>
2024-05-04 04:01:02 -0600

    migrate to new version, fixed icons issue

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-
+import icon from 'astro-icon';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://leonardoserrano.site',
-    integrations: [tailwind({ applyBaseStyles: false }), react(), sitemap()],
+    integrations: [
+        tailwind({ applyBaseStyles: false }),
+        react(),
+        sitemap(),
+        icon(),
+    ],
 });
